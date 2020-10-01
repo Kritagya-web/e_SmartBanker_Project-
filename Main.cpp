b2561b508bf4a423658d6778697cbec695484751
henishjindal <71707119+henishjindal@users.noreply.github.com>
2020-10-01 03:04:22 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+  cout << "Hello World!" << '\n';
+}
+int32_t main() {
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL); 
+  solve();
+}
